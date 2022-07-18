c1f8ef8d501fabbb82f868550f6340f68477f575
idanwasserman <idan.wasserman@s.afeka.ac.il>
2022-07-18 14:31:56 +0300

    Added timestamp to messages

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MSG = '!DISCONNECT!'
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 5050
+ADDR = (HOST, PORT)
+
+
+class MyClient:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(ADDR)
+
+    def send_data(self, data):
+        data = data.encode(FORMAT)
+        data_length = len(data)
+        send_length = str(data_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        self.client.send(send_length)
+        self.client.send(data)
+        recv_data = self.client.recv(1024).decode(FORMAT)
+        return recv_data
